5af2233332d8283ab9949455a0bce2014d1e48d1
Alberto Garcia <alb.garciam@gmail.com>
2019-11-01 10:56:44 +0100

    Adding linters and create middlewares directory to extract middlewares

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+import TokenMiddleware from './token';
+import ValidationMiddleware from './validation';
+
+export { TokenMiddleware, ValidationMiddleware };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
-import User from "./user";
-import Token from "./token";
-import Validation from "./validation";
-import Profile from "./profile";
-export { User, Token, Validation, Profile };
+import User from './user';
+import Profile from './profile';
+
+export { User, Profile };
