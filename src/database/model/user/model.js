5adb521dc94bf2e209dccc06dc811655839075ba
Alberto Garcia <alb.garciam@gmail.com>
2019-11-09 10:54:54 +0100

    fix possible import issues

--- src/database/model/user/model.js
+++ src/database/model/user/model.js
@@ -1,5 +1,5 @@
-import { Schema, model } from "mongoose";
-import { Error } from "../../../dto";
+import { Schema, model } from 'mongoose';
+import { Error } from '../../../dto';
 
 const UserSchema = Schema(
   {
@@ -19,69 +19,49 @@ const UserSchema = Schema(
       index: true
     },
     salt: { type: String, unique: true, required: true },
-    dateAdded: { type: "Date", default: Date.now, required: true }
+    dateAdded: { type: 'Date', default: Date.now, required: true }
   },
-  { collection: "User" }
+  { collection: 'User' }
 );
 
-const UserModel = model("User", UserSchema);
+const UserModel = model('User', UserSchema);
 
 UserModel.getByEmail = async email => {
   try {
-    return await UserModel.findOne({ email: email }).select(
-      "email password cuid dateAdded salt -_id"
-    );
+    return await UserModel.findOne({ email }).select('email password cuid dateAdded salt -_id');
   } catch (err) {
-    throw ErrorDTO.DTO(
-      ErrorDTO.CODE_SERVER_ERROR,
-      ErrorDTO.ECODE_DATABASE_ERROR,
-      err.message
-    );
+    throw Error.DTO(Error.CODE_SERVER_ERROR, Error.ECODE_DATABASE_ERROR, err.message);
   }
 };
 
 UserModel.getByCuid = async cuid => {
   try {
-    return await UserModel.findOne({ cuid: cuid }).select(
-      "email password cuid dateAdded salt -_id"
-    );
+    return await UserModel.findOne({ cuid }).select('email password cuid dateAdded salt -_id');
   } catch (err) {
-    throw ErrorDTO.DTO(
-      ErrorDTO.CODE_SERVER_ERROR,
-      ErrorDTO.ECODE_DATABASE_ERROR,
-      err.message
-    );
+    throw Error.DTO(Error.CODE_SERVER_ERROR, Error.ECODE_DATABASE_ERROR, err.message);
   }
 };
 
-UserModel.createUser = async model => {
+UserModel.createUser = async user => {
   try {
-    return await model.save();
+    return await user.save();
   } catch (err) {
-    if (err.code == 11000) {
+    if (err.code === 11000) {
       throw Error.DTO(
         Error.CODE_AUTHORIZATION_ERROR,
         Error.ECODE_DUPLICATED_ITEM,
         Error.MSG_DUPLICATED_ITEM
       );
     }
-    throw Error.DTO(
-      Error.CODE_SERVER_ERROR,
-      Error.ECODE_DATABASE_ERROR,
-      err.errmsg
-    );
+    throw Error.DTO(Error.CODE_SERVER_ERROR, Error.ECODE_DATABASE_ERROR, err.errmsg);
   }
 };
 
 UserModel.updatePassword = async (cuid, password) => {
   try {
-    return await UserModel.updateOne({ cuid: cuid }, { password: password });
+    return await UserModel.updateOne({ cuid }, { password });
   } catch (err) {
-    throw ErrorDTO.DTO(
-      ErrorDTO.CODE_SERVER_ERROR,
-      ErrorDTO.ECODE_DATABASE_ERROR,
-      err.message
-    );
+    throw Error.DTO(Error.CODE_SERVER_ERROR, Error.ECODE_DATABASE_ERROR, err.message);
   }
 };
 
