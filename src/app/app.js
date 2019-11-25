c938b848ef1c8a7cc14ae58fde4310416d4c2915
Alberto Garcia <alb.garciam@gmail.com>
2019-11-25 21:24:22 +0100

    Add logs

--- src/app/app.js
+++ src/app/app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+  console.error(err.stack);
   if (!_.isNil(err) && !_.isNil(err.code)) {
     res.status(err.code).json(err);
   } else {
