b2117c6df83607eb93c2389a02896141b5dfffff
Alberto Garcia <alb.garciam@gmail.com>
2019-11-30 11:41:18 +0100

    Adding documentation

--- src/dto/index.js
+++ src/dto/index.js
@@ -1,3 +1,11 @@
+/**
+ * @namespace Routes
+ * @property {module:routes/user} UserRouter Prepares routes for users
+ * @property {module:routes/profile} ProfileRouter Prepares routes for profiles
+ * @property {module:routes/local} LocalRouter Prepares routes for locals
+ * @property {module:routes/search} SearchRouter Prepares routes for searchs
+ * @property {module:routes/notification} NotificationRouter Prepares routes for notifications
+ */
 import * as Error from './error';
 import UserDTO from './user';
 import LocationDTO from './location';
